89ffb5ecdb53e64b1113c762f5d16f8115637455
keito0317 <128699301+keito0317@users.noreply.github.com>
2023-07-15 16:53:39 +0900

    img

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,38 +1,31 @@
 class RoomsController < ApplicationController
+  protect_from_forgery
   def index
     @rooms = Room.all
   end
 
   def new
     @room = Room.new
-    @comment = Comment.new
   end
 
   def create
-    @room = Room.new(params.require(:room).permit(:room_name, :details, :price, :address))
+    @room = Room.new(params.require(:room).permit(:image, :room_name, :details, :price, :address))
     if @room.save
       flash[:notice] = "新規登録しました"
       redirect_to :rooms
     else
       render "new"
     end
-    @comment = Comment.create params.require(:comment).permit(:content, :image)
-    redirect_to @room
   end
 
   def show
     @room = Room.find(params[:id])
-    @comment = Comment.find(params[:id])
   end
 
   def edit
-    @comment = Comment.find(params[:id]) 
   end
 
   def update
-    @comment = Comment.find(params[:id])
-    @comment.update params.require(:comment).permit(:content, :image) # POINT
-    redirect_to @comment
   end
 
   def destoroy
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_04_121842) do
+ActiveRecord::Schema.define(version: 2023_07_15_064757) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2023_07_04_121842) do
     t.string "persons"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
