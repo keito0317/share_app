5baa6959277a81e1f8dcf46aa523bfc39eeb7721
keito0317 <128699301+keito0317@users.noreply.github.com>
2023-07-15 21:14:46 +0900

    image

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :comments
   resources :rooms
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
